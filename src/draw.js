3a639166de6b084e32d49cbfd2f91bb8cdd2f118
h515 <34015597+h515@users.noreply.github.com>
2024-08-06 17:09:49 +0800

    Update draw.js

--- src/draw.js
+++ src/draw.js
@@ -23,7 +23,7 @@ async function draw() {
     headers,
     method: 'POST',
     credentials: 'include'
-  }).then((res) => res.json());
+  }).then((res) => res);
 
   if (res.err_no !== 0) return Promise.reject('免费抽奖异常，接口调用异常！');
 
